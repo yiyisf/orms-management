faf7f3403059e7eeecbafc53d376d0fd36e5d18d
zhang <yiyi119120@gmail.com>
2020-06-06 20:51:53 +0800

    add discovery service

--- src/main/java/com/srvbot/forms/Application.java
+++ src/main/java/com/srvbot/forms/Application.java
@@ -4,6 +4,7 @@ import com.srvbot.forms.domain.ChatMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.UnicastProcessor;
@@ -12,6 +13,7 @@ import reactor.core.publisher.UnicastProcessor;
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
